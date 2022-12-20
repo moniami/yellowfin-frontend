b13290bb9834a3ada2a37530d2108a07b9c5c2bd
moniami <moniami@amazon.it>
2022-12-20 11:40:09 +0100

    start

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export IP_ADDR=$(hostname -f)
+yum update -y
+yum install java-1.8.0-openjdk-devel -y
+yum install git -y
+wget https://mirrors.estointernet.in/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+tar -xvf apache-maven-3.6.3-bin.tar.gz
+mv apache-maven-3.6.3 /opt/
+M2_HOME='/opt/apache-maven-3.6.3'
+PATH="$M2_HOME/bin:$PATH"
+export PATH
+git clone https://github.com/moniami/yellowfin-frontend
+cd yellowfin-frontend
+mvn clean install
+mvn spring-boot:run
