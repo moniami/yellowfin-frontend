ad8aa5e7f1dbc8e6c752bd1df1235c55c62b70aa
moniami <moniami@amazon.it>
2023-01-03 14:14:23 +0100

    mapcontroller

--- src/main/java/com/example/servingwebcontent/MapController.java
+++ src/main/java/com/example/servingwebcontent/MapController.java
@@ -1,16 +1,58 @@
 package com.example.servingwebcontent;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.*;
+import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.IOException;
+import java.util.Properties;
+
+
 @Controller
 public class MapController {
+	String configFilePath = "src/main/resources/config.ini";
 	@GetMapping("/map")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="User") String name, Model model) {
-		model.addAttribute("name", name);
-		return "map";
+	public String greeting() throws IOException {
+
+		try {
+			FileInputStream propsInput = new FileInputStream(configFilePath);
+			Properties prop = new Properties();
+			prop.load(propsInput);
+
+			String BACKEND_URL;
+			BACKEND_URL = prop.getProperty("BACKEND_URL");
+			URL url = new URL("http://"+BACKEND_URL +":8080/currentUser");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+
+			if (conn.getResponseCode() != 200) {
+				error(conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+			String response;
+			while ((response = br.readLine()) != null) {
+				System.out.println(response);
+			}
+			return response;
+
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GetMapping("/error")
+	public Integer error(int error){
+		return error;
 	}
 
 }
--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -1,8 +1,6 @@
 package com.example.servingwebcontent;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,21 +9,7 @@ import java.util.Properties;
 @SpringBootApplication
 public class ServingWebContentApplication {
 
-    public static void main(String[] args) throws FileNotFoundException {
-
-        String configFilePath = "src/main/resources/config.ini";
-        try {
-            FileInputStream propsInput = new FileInputStream(configFilePath);
-            Properties prop;
-            prop = new Properties();
-            prop.load(propsInput);
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public static void main(String[] args) throws IOException {
 
         SpringApplication.run(ServingWebContentApplication.class, args);
     }
