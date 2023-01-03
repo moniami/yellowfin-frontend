e1bf026f56fce7b33d5196c0c82814839d9d376f
moniami <moniami@amazon.it>
2023-01-03 16:15:19 +0100

    aggiunto parametro html

--- src/main/java/com/example/servingwebcontent/MapController.java
+++ src/main/java/com/example/servingwebcontent/MapController.java
@@ -1,24 +1,23 @@
 package com.example.servingwebcontent;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.io.*;
-import java.net.*;
 import java.io.BufferedReader;
+import org.springframework.ui.Model;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.io.IOException;
 import java.util.Properties;
-
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MapController {
 	String configFilePath = "src/main/resources/config.ini";
 	@GetMapping("/map")
-	public String greeting() throws IOException {
+	public String mapping(Model model) throws IOException {
 
 		try {
 			FileInputStream propsInput = new FileInputStream(configFilePath);
@@ -28,31 +27,43 @@ public class MapController {
 			String BACKEND_URL;
 			BACKEND_URL = prop.getProperty("BACKEND_URL");
 			URL url = new URL("http://"+BACKEND_URL +":8080/currentUser");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
+			try {
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
 
-			if (conn.getResponseCode() != 200) {
-				error(conn.getResponseCode());
-			}
+				if (conn.getResponseCode() != 200) {
+					model.addAttribute("key", "Guest");
+					return "map";
+				}
+				else {
+					BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+					StringBuilder jsonString = new StringBuilder();
+					String readAPIResponse = "";
+					while((readAPIResponse = br.readLine()) != null){
+						jsonString.append(readAPIResponse);
+					}
+					JSONObject jsonObj = new JSONObject(jsonString.toString());
+					System.out.println(jsonString);
+					System.out.println("---------------------------");
+					System.out.println(jsonObj);
+					model.addAttribute("key", (String) jsonObj.get("user"));
+					return  "map";
+				}
 
-			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-			String response;
-			while ((response = br.readLine()) != null) {
-				System.out.println(response);
+			} catch (Exception e){
+				System.out.println(e);
+				model.addAttribute("key", "User");
+				return  "map";
 			}
-			return response;
 
 		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
+			System.out.println(e);
+			return "map";
 
 		} catch (IOException e) {
-			throw new RuntimeException(e);
+			System.out.println(e);
+			return "map";
 		}
-	}
-
-	@GetMapping("/error")
-	public Integer error(int error){
-		return error;
 	}
 
 }
